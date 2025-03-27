74fdc38ab3651f4241a387529aa4388ed9c0254f
Karl Gustav <kkgg84+git@gmail.com>
2025-03-28 00:21:05 +0100

    calculator_test: simplified the tests and testing whole file now

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -3,131 +3,88 @@ package calculator
 import (
 	"context"
 	"encoding/xml"
+	"fmt"
 	"os"
-	"slices"
 	"testing"
 	"time"
 
-	"github.com/karl-gustav/power_price/common"
 	"github.com/karl-gustav/power_price/currency"
 )
 
 var exchangeRate = currency.ExchangeRate{
-	Rate: 10,
+	Rate: 1,
 }
 
-func Test60mResolution(t *testing.T) {
-	first60MinDate := "2025-01-22T00:00:00+01:00"
-	first60MinPrice := 47.14
-	last60MinDate := "2025-01-22T23:00:00+01:00"
-	last60MinPrice := 58.51
-	xml60min, err := os.ReadFile("./testdata/60m.xml")
+func Test60m(t *testing.T) {
+	prices := []float64{47.14, 40.6, 40.64, 40.75, 41.16, 50.12, 122.94, 200.98, 224, 193.92, 173.51, 167.58, 160.22, 165.07, 174.7, 189.99, 193.27, 202.93, 175.19, 162.23, 129.99, 123.82, 103.58, 58.51}
+	xmlData, err := os.ReadFile("./testdata/60m.xml")
 	if err != nil {
 		t.Errorf("unexpected error %v", err)
 	}
 
 	var powerPricesXML PublicationMarketDocument
-	err = xml.Unmarshal(xml60min, &powerPricesXML)
+	err = xml.Unmarshal(xmlData, &powerPricesXML)
 	if err != nil {
 		t.Errorf("unexpected error %v", err)
 	}
 	powerPrices := CalculatePriceForcast(context.Background(), powerPricesXML, exchangeRate)
 
-	var keys []string
-	for key := range powerPrices {
-		keys = append(keys, key)
-	}
-	slices.Sort(keys)
-	if keys[0] != first60MinDate {
-		t.Errorf("first date is expected to be %s, was %s", first60MinDate, keys[0])
-	}
-	if keys[len(keys)-1] != last60MinDate {
-		t.Errorf("last date is expected to be %s, was %s", last60MinDate, keys[len(keys)-1])
-	}
-	if len(powerPrices) != 24 {
-		t.Errorf("expected 24 values in parsed power prices was %d", len(powerPrices))
-	}
-
-	first60MinFrom, _ := time.Parse(time.RFC3339, first60MinDate)
-	first60MinTo := first60MinFrom.Add(1 * time.Hour)
-	if !powerPrices[first60MinDate].From.Equal(first60MinFrom) {
-		t.Errorf("expected first valid_from to be %s, was %s", first60MinFrom.In(common.Loc), powerPrices[first60MinDate].From)
-	}
-	if !powerPrices[first60MinDate].To.Equal(first60MinTo) {
-		t.Errorf("expected first valid_to to be %s, was %s", first60MinTo.In(common.Loc), powerPrices[first60MinDate].To)
-	}
-
-	last60MinFrom, _ := time.Parse(time.RFC3339, last60MinDate)
-	last60MinTo := last60MinFrom.Add(1 * time.Hour)
-	if !powerPrices[last60MinDate].From.Equal(last60MinFrom) {
-		t.Errorf("expected last valid_from to be %s, was %s", last60MinFrom.In(common.Loc), powerPrices[last60MinDate].From)
-	}
-	if !powerPrices[last60MinDate].To.Equal(last60MinTo) {
-		t.Errorf("expected last valid_to to be %s, was %s", last60MinTo.In(common.Loc), powerPrices[last60MinDate].To)
-	}
-
-	if powerPrices[first60MinDate].PriceMWhEUR != first60MinPrice {
-		t.Errorf("expected price to be %f, was %f", first60MinPrice, powerPrices[first60MinDate].PriceMWhEUR)
-	}
-	if powerPrices[last60MinDate].PriceMWhEUR != last60MinPrice {
-		t.Errorf("expected price to be %f, was %f", last60MinPrice, powerPrices[last60MinDate].PriceMWhEUR)
+	for hour := range 24 {
+		tsString := fmt.Sprintf("2025-01-22T%02d:00:00+01:00", hour)
+		startTime, _ := time.Parse(time.RFC3339, tsString)
+		endTime := startTime.Add(1 * time.Hour)
+		pricePoint := powerPrices[tsString]
+		if pricePoint.PriceMWhEUR != prices[hour] {
+			t.Errorf("expected the price for %s to be %f, but it was %f", tsString, prices[hour], pricePoint.PriceMWhEUR)
+		}
+		if pricePoint.PriceKWhNOK != prices[hour]/1000 {
+			t.Errorf("expected prices kwh nok to be %f, was %f", pricePoint.PriceKWhNOK, prices[hour])
+		}
+		if !pricePoint.From.Equal(startTime) {
+			t.Errorf("expected start time to be %s, was %s", pricePoint.From, startTime)
+		}
+		if !pricePoint.To.Equal(endTime) {
+			t.Errorf("expected end time to be %s, was %s", pricePoint.To, endTime)
+		}
+		if pricePoint.ExchangeRate != exchangeRate.Rate {
+			t.Errorf("expected exchange rate to be %f, was %f", exchangeRate.Rate, pricePoint.ExchangeRate)
+		}
 	}
 }
 
-func Test15mResolution(t *testing.T) {
-	first15MinDate := "2025-02-23T00:00:00+01:00"
-	first15MinPrice := 48.74
-	last15MinDate := "2025-02-23T23:00:00+01:00"
-	last15MinPrice := 47.39
-	xml15min, err := os.ReadFile("./testdata/15m.xml")
+func Test15m(t *testing.T) {
+	prices := []float64{48.74, 48.66, 48.58, 48.56, 48.64, 48.84, 49.5, 49.92, 50.81, 51.79, 51.25, 50.58, 48.27, 47.41, 47.44, 49.14, 51.15, 55.02, 54.85, 53.46, 51.13, 49.74, 48.83, 47.39}
+	xmlData, err := os.ReadFile("./testdata/15m.xml")
 	if err != nil {
 		t.Errorf("unexpected error %v", err)
 	}
 
 	var powerPricesXML PublicationMarketDocument
-	err = xml.Unmarshal(xml15min, &powerPricesXML)
+	err = xml.Unmarshal(xmlData, &powerPricesXML)
 	if err != nil {
 		t.Errorf("unexpected error %v", err)
 	}
 	powerPrices := CalculatePriceForcast(context.Background(), powerPricesXML, exchangeRate)
 
-	var keys []string
-	for key := range powerPrices {
-		keys = append(keys, key)
-	}
-	slices.Sort(keys)
-	if keys[0] != first15MinDate {
-		t.Errorf("first date is expected to be %s, was %s", first15MinDate, keys[0])
-	}
-	if keys[len(keys)-1] != last15MinDate {
-		t.Errorf("last date is expected to be %s, was %s", last15MinDate, keys[len(keys)-1])
-	}
-	if len(powerPrices) != 24 {
-		t.Errorf("expected 24 values in parsed power prices was %d", len(powerPrices))
-	}
-
-	first15MinFrom, _ := time.Parse(time.RFC3339, first15MinDate)
-	first15MinTo := first15MinFrom.Add(1 * time.Hour)
-	if !powerPrices[first15MinDate].From.Equal(first15MinFrom) {
-		t.Errorf("expected valid_from to be %s, was %s", first15MinFrom.In(common.Loc), powerPrices[first15MinDate].From)
-	}
-	if !powerPrices[first15MinDate].To.Equal(first15MinTo) {
-		t.Errorf("expected valid_to to be %s, was %s", first15MinTo.In(common.Loc), powerPrices[first15MinDate].To)
-	}
-
-	last15MinFrom, _ := time.Parse(time.RFC3339, last15MinDate)
-	last15MinTo := last15MinFrom.Add(1 * time.Hour)
-	if !powerPrices[last15MinDate].From.Equal(last15MinFrom) {
-		t.Errorf("expected first valid_from to be %s, was %s", last15MinFrom.In(common.Loc), powerPrices[last15MinDate].From)
-	}
-	if !powerPrices[last15MinDate].To.Equal(last15MinTo) {
-		t.Errorf("expected first valid_to to be %s, was %s", last15MinTo.In(common.Loc), powerPrices[last15MinDate].To)
-	}
-
-	if powerPrices[first15MinDate].PriceMWhEUR != first15MinPrice {
-		t.Errorf("expected last price to be %f, was %f", first15MinPrice, powerPrices[first15MinDate].PriceMWhEUR)
-	}
-	if powerPrices[last15MinDate].PriceMWhEUR != last15MinPrice {
-		t.Errorf("expected last price to be %f, was %f", last15MinPrice, powerPrices[last15MinDate].PriceMWhEUR)
+	for hour := range 24 {
+		tsString := fmt.Sprintf("2025-02-23T%02d:00:00+01:00", hour)
+		startTime, _ := time.Parse(time.RFC3339, tsString)
+		endTime := startTime.Add(1 * time.Hour)
+		pricePoint := powerPrices[tsString]
+		if pricePoint.PriceMWhEUR != prices[hour] {
+			t.Errorf("expected the price for %s to be %f, but it was %f", tsString, prices[hour], pricePoint.PriceMWhEUR)
+		}
+		if pricePoint.PriceKWhNOK != prices[hour]/1000 {
+			t.Errorf("expected prices kwh nok to be %f, was %f", pricePoint.PriceKWhNOK, prices[hour])
+		}
+		if !pricePoint.From.Equal(startTime) {
+			t.Errorf("expected start time to be %s, was %s", pricePoint.From, startTime)
+		}
+		if !pricePoint.To.Equal(endTime) {
+			t.Errorf("expected end time to be %s, was %s", pricePoint.To, endTime)
+		}
+		if pricePoint.ExchangeRate != exchangeRate.Rate {
+			t.Errorf("expected exchange rate to be %f, was %f", exchangeRate.Rate, pricePoint.ExchangeRate)
+		}
 	}
 }
